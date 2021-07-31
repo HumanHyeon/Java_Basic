bb595f72e192e8ba598a46e4ef7aff33b6200ed0
HumanHyeon <sehyeon1997@gmail.com>
2021-07-31 22:38:40 +0900

    make Chapter2

---
+++ Chapter1/src/ch04/BinaryTest.java
@@ -0,0 +1,18 @@
+package ch04;
+
+public class BinaryTest {
+
+	public static void main(String[] args) {
+		int num = 10;
+		int binaryNum = 0B1010;
+		int octNum = 012;
+		int hexNum = 0xA;
+		
+		System.out.println(num);
+		System.out.println(binaryNum);
+		System.out.println(octNum);
+		System.out.println(hexNum);
+
+	}
+
+}
