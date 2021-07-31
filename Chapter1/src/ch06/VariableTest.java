7ef8590ed6eff33d8eb7534ac43865d3f6d8e315
HumanHyeon <sehyeon1997@gmail.com>
2021-07-31 23:58:44 +0900

    modi Ch1

---
+++ Chapter1/src/ch06/VariableTest.java
@@ -0,0 +1,15 @@
+package ch06;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		byte bnum = -128;
+		//byte bnum = 128;
+		
+		int num = 123456789;
+		
+		System.out.println(bnum);
+		System.out.println(num);
+	}
+
+}
